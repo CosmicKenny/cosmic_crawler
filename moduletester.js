6245ceb95d0a9916a138fb327750a9c14aed3b3d
Kenny Saw <Kenny.Saw@sg.ey.com>
2019-05-17 11:35:50 +0800

    minor fixes on on csv generator

--- moduletester.js
+++ moduletester.js
@@ -11,5 +11,5 @@ const jsonToCsv = require('./jsonToCsv.js');
 
 
 jsonToCsv.jsonToCsv('reports/crawledURLs.json', ['url'], 'reports/crawledURLs.csv', 'url');
-jsonToCsv.jsonToCsv('reports/crawledPages.json', ['url', 'title', 'description', 'lastUpdatedText'], 'reports/crawledURLs.csv');
+jsonToCsv.jsonToCsv('reports/crawledPages.json', ['url', 'title', 'description', 'lastUpdatedText'], 'reports/crawledPages.csv');
 jsonToCsv.jsonToCsv('reports/brokenLinks.json', ['source', 'url', 'code'], 'reports/brokenLinks.csv');
